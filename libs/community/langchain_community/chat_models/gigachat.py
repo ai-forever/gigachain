d3353f2e43c43f926f52aca9494cfb22abd4e31c
Dmitry Labazkin <labdmitriy@gmail.com>
2024-07-19 16:45:30 +0300

    Implement conversion from langchain to gigachat messages for AI message with tool calling

--- libs/community/langchain_community/chat_models/gigachat.py
+++ libs/community/langchain_community/chat_models/gigachat.py
@@ -157,7 +157,12 @@ def _convert_message_to_dict(message: BaseMessage) -> gm.Messages:
         kwargs["role"] = MessagesRole.USER
         kwargs["content"] = message.content
     elif isinstance(message, AIMessage):
-        function_call = message.additional_kwargs.get("function_call", None)
+        if function_call := getattr(message, "tool_calls", None):
+            for function in function_call:
+                if "args" in function:
+                    function["arguments"] = function.pop("args")
+        else:
+            function_call = message.additional_kwargs.get("function_call", None)
         if isinstance(function_call, list):
             if len(function_call) > 0:
                 function_call = function_call[0]
